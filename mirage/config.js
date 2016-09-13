9212449ca17a8cf79bf4197290ba9ebc6056088c
Curtis Ekstrom <curtis@travis-ci.org>
2016-09-13 10:19:31 +0200

    Move dashboard feature flag to API response
    This fixes some incompatibilities with the two approaches, e.g. weird redirect errors when transitioning to `/dashboard`.

--- mirage/config.js
+++ mirage/config.js
@@ -273,7 +273,7 @@ export default function () {
         {
           name: 'Dashboard',
           description: 'UX improvements over the current implementation',
-          enabled: false
+          enabled: true
         },
         {
           name: 'Make header pink',
